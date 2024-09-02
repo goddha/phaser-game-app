f5e1bc7b0e12d2f45c49ec0a8e9b912b84421341
goddha <goddha@outlook.com>
2024-09-02 21:08:03 +0700

    re move hilo

--- src/components/games/HiLow/utils/Box.ts
+++
@@ -1,7 +0,0 @@
-export default class Box extends Phaser.GameObjects.Sprite {
-  constructor(scene: Phaser.Scene, x: number, y: number, texture: string) {
-    super(scene, x, y, texture)
-    this.setDisplaySize(196, 77)
-    this.scene.add.existing(this)
-  }
-}
--- src/components/games/HiLow/utils/Result.ts
+++
@@ -1,7 +0,0 @@
-export default class Result extends Phaser.GameObjects.Sprite {
-  constructor(scene: Phaser.Scene, x: number, y: number, result: string) {
-    super(scene, x, y, result)
-    this.setInteractive()
-    this.scene.add.existing(this)
-  }
-}
